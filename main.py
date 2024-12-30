e39aa314c68c9ede289655892ea2528b4a1b9d2c
Ovidio Calvet <calvetovidio@gmail.com>
2024-12-30 11:20:32 -0500

    new: adds movement and the second paddle to the screen

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
-player_one = Paddle()
-player_one.position = 'right'
-
 screen.setup(width=900, height=600)
 screen.bgcolor('#373D20')
 
+player_one = Paddle(position='right')
+player_two = Paddle(position='left')
+
+ball = Ball()
+
 screen.listen()
+screen.onkeypress(player_one.up, 'w')
+screen.onkeypress(player_one.down, 's')
 
+screen.onkeypress(player_two.up, 'i')
+screen.onkeypress(player_two.down, 'k')
 
+game_is_not_over = True
+
+while game_is_not_over:
+
+    pass
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,24 +1,31 @@
 from turtle import Turtle
 
+MOVEMENT_INCREMENT = 20
+
 class Paddle(Turtle, ):
 
-    def __init__(self):
+    def __init__(self, position):
         
         super().__init__()
 
         self.hideturtle()
-        self.create_paddle()
-
-        self.position
-        self.movement_keys = []
+        self.position = position
+        self.paddle = self.create_paddle()
+        
 
     def create_paddle(self):
 
-        paddle = Turtle('square')
-        paddle.shapesize(stretch_len=.5, stretch_wid=4)
-        paddle.color('#ADC178')
-        paddle.penup()
-        paddle.speed(0)
+        new_paddle = Turtle('square')
+        new_paddle.shapesize(stretch_len=.5, stretch_wid=4)
+        new_paddle.color('#ADC178')
+        new_paddle.penup()
+        new_paddle.speed(0)
 
-        if self.position == 'right': paddle.goto(425,0)
-        else: paddle.goto(-425, 0)
+        if self.position == 'right': new_paddle.goto(425,0)
+        else: new_paddle.goto(-425, 0)
+
+        return new_paddle
+
+    def up(self): self.paddle.goto(self.paddle.xcor(), self.paddle.ycor() + MOVEMENT_INCREMENT)
+
+    def down(self): self.paddle.goto(self.paddle.xcor(), self.paddle.ycor() - MOVEMENT_INCREMENT)
